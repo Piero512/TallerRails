3a4c95125aac5a962941decdfcd59dfcbf7fb630
Vivi Laurido <vlaurido@espol.edu.ec>
2019-01-14 14:22:43 -0500

    fixed top10 query

--- app/controllers/top10_controller.rb
+++ app/controllers/top10_controller.rb
@@ -1,5 +1,5 @@
 class Top10Controller < ApplicationController
   def index
-     @top10 = Movie.order("rating DESC").limit(10)
+    @top10 = View.joins(:movie).select('movies.title , views.rating').order("views.rating DESC").limit(10)
   end
 end
